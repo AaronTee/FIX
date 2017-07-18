aefef8c8c02f3be975811ea2d065e16e2aec38ea
Aaron Tee <tee.qiiyang@syntrinos.com.my>
2017-07-18 08:47:52 +0800

    wip

---
+++ FIX.Web/Controllers/MatchingBonusController.cs
@@ -0,0 +1,62 @@
+﻿using FIX.Service;
+using FIX.Service.Entities;
+using FIX.Web.Extensions;
+using FIX.Web.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIX.Web.Controllers
+{
+    public class MatchingBonusController : Controller
+    {
+        private IUserService _userService;
+
+        // GET: MatchingBonus
+        public ActionResult Index(IUserService userService)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            //Find all downline users
+            MatchingBonusListViewModels model = new MatchingBonusListViewModels
+            {
+                UserId = 
+            };
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+
+        }
+
+        public JsonResult MatchingBonusList(int offset, int limit, string sort, string order)
+        {
+            var queryableList = _userService.GetReferralChildren(User.Identity.GetUserId<int>());
+            var allRowCount = queryableList.Count();
+
+            queryableList = queryableList.PaginateList("Generation", sort, order, offset, limit);
+
+            var rowsResult = queryableList.ToList()
+                .Select(x => new MatchingBonusListViewModels()
+                {
+                    MatchingBonusId = 
+                });
+
+            var model = new
+            {
+                total = allRowCount,
+                rows = rowsResult
+            };
+
+
+            return Json(new { }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ FIX.Web/Models/MatchingBonusModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIX.Web.Models
+{
+    public class MatchingBonusListViewModels{
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Package { get; set; }
+        public int Generation { get; set; }
+        public decimal? BonusAmount { get; set; }
+    }
+
+}
